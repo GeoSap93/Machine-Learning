6817d545c55ffbd8d1bd05d78451e6c7a0385bef
Georgia Sapsani <134007087+GeoSap93@users.noreply.github.com>
2023-12-18 13:19:31 +0000

    Add files via upload

--- Object Recognition_Final.py
+++ Object Recognition_Final.py
@@ -27,19 +27,29 @@ def detect_objects(image_path):
     detected_objects = []
     object_count = {obj: 0 for obj in objects_to_detect}
 
+    #Objects we detect that are not in our list of objects
+    other_objects= []
+
     for eachObject in detections:
         object_name = eachObject["name"].lower()
+        # Store objects from the specified list by using .append() method to add the items in the list detected_objects
         detected_objects.append(object_name)
 
         # Count objects from the specified list
         if object_name in objects_to_detect:
             object_count[object_name] += 1
+        else:
+        # Store objects that are not from the specified list by using .append() method in the list other_objects
+            other_objects.append(object_name)
 
-    return detected_objects, object_count
+    # The len() funtion counts the number of items in the list "other_objects"
+    object_count["Other"] = len(other_objects)
+
+    return detected_objects, object_count, other_objects
 
 # Example usage
 image_path = "Machine_Learning_Dataset"
-detected_objects, object_count = detect_objects(image_path)
+detected_objects, object_count, other_objects = detect_objects(image_path)
 
 print("Detected Objects:")
 print(detected_objects)
@@ -47,3 +57,6 @@ print(detected_objects)
 print("\nObject Count:")
 for obj, count in object_count.items():
     print(f"{obj.capitalize()}: {count}")
+
+print ("\nOther Objects Detected not present in the list:")
+print (other_objects)
